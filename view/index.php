e37821e8905e37ed0ac11e75f415c59d5c75dba3
ukp9131 <azb4472@naver.com>
2026-04-16 16:32:28 +0900

    Update index.php

--- view/index.php
+++ view/index.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="_.php?file=index.css&v=1">
 <div class="ukpb__content">
-    hello world!
+    hello world!!!
 </div>
 <script type="module" src="_.php?file=index.js&v=1"></script>
